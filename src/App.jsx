0b32012e2a8325db89efa51b42aa9dc642d5a60a
OXplakhoa <OXplakhoa@gmail.com>
2024-10-25 16:27:27 +0700

    Add useNetwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
+import { useNetwork } from './utils/useNetwork';
+
 
 function App() {
-  const fadeInH1 = useFadeIn(2, 2);
-  const fadeInP = useFadeIn(5, 5);
+  const handleNetworkChange =  (online) => {
+    console.log(online ? "We On Fire!" : "We Down To The Moon");
+  }
+  const status = useNetwork(handleNetworkChange);
   return (
     <>
-      <h1 {...fadeInH1}>Hello</h1>
-      <p {...fadeInP}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, odit! Ab accusamus aspernatur voluptates voluptatem dolorem in iste quod neque molestiae, at quidem vel excepturi eligendi corporis sapiente porro eum?</p>
+      <h1>{status ? "Online" : "Offline"}</h1>
     </>
   );
 }
