e639a388f5ca811a7b6f30dda089344e199e4370
OXplakhoa <OXplakhoa@gmail.com>
2024-10-25 21:20:22 +0700

    Add useFullscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css';
-import { useScroll } from './utils/useScroll';
-
+import './App.css'
+import { useFullscreen } from './utils/useFullscreen';
 
 function App() {
-  const {y} = useScroll();
+  const onFulls = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull, isFull } = useFullscreen(onFulls);
+
   return (
-    <div style={{height: "100vh"}}>
-      <h1 style={{color: y > 1 ? "red" : "blue" }}>Hello</h1>
+    <div style={{ height: "100vh" }}>
+      <div ref={element}>
+        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="" width="250" />
+        {isFull && <button onClick={exitFull}>Exit</button>}
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/utils/useFullscreen.jsx
@@ -0,0 +1,66 @@
+import { useRef, useState, useEffect } from 'react';
+
+export const useFullscreen = (callback) => {
+  const element = useRef();
+  const [isFull, setIsFull] = useState(false);
+
+  const runCallback = (isFull) => {
+    if (callback && typeof callback === 'function') {
+      callback(isFull);
+    }
+  };
+
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullscreen) {
+        element.current.mozRequestFullscreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      } else {
+        console.log("Your browser (maybe) does not support fullscreen.");
+      }
+      setIsFull(true);
+      runCallback(true);
+    }
+  };
+
+  const exitFull = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+      document.mozCancelFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else {
+      console.log("Your browser (maybe) does not support exiting fullscreen.");
+    }
+    setIsFull(false);
+    runCallback(false);
+  };
+
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      setIsFull(!!document.fullscreenElement);
+    };
+
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    document.addEventListener('mozfullscreenchange', handleFullscreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
+    document.addEventListener('msfullscreenchange', handleFullscreenChange);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', handleFullscreenChange);
+      document.removeEventListener('mozfullscreenchange', handleFullscreenChange);
+      document.removeEventListener('webkitfullscreenchange', handleFullscreenChange);
+      document.removeEventListener('msfullscreenchange', handleFullscreenChange);
+    };
+  }, []);
+
+  return { element, triggerFull, exitFull, isFull };
+};
